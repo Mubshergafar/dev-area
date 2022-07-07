5e0019fd4c0d7be4818f41b835aa23888de95e7c
mubshergafar <mubsher.gafar@outlook.com>
2022-07-07 20:13:39 +0400

    adding auth route to register an user and assign a jwt token , login a user

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
